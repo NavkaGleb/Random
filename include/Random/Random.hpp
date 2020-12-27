5cb6c4c40fca804b330600ed86648b8ca261d5f6
NavkaGleb <navka.gleb@gmail.com>
2020-12-27 04:01:17 +0200

    Random: Add Random.cpp file

--- include/Random/Random.hpp
+++ include/Random/Random.hpp
@@ -79,20 +79,6 @@ namespace Ng {
         return GetInstance().GetImpl<T>(left, right);
     }
 
-    // Member constructor
-    Random::Random() :
-        m_SeedSequence({
-            m_RandomDevice(),
-            static_cast<unsigned int>(std::chrono::steady_clock::now().time_since_epoch().count())
-        }),
-        m_MersenneTwister(m_SeedSequence) { }
-
-    // Member static methods
-    Random& Random::GetInstance() {
-        static Random instance;
-        return instance;
-    }
-
     // Member methods
     template <>
     bool Random::GetImpl<bool>(float probability) {
